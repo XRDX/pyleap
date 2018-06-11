5f68271b2a2cccfb83376d0690fcc5ba8a7dfdbf
Vic <305880887@qq.com>
2018-06-11 14:58:18 +0800

    add preinstall pyglet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyleap",
-    version="0.0.2",
+    version="0.0.3",
     author="Vic Wang",
     author_email="305880887@qq.com",
     description='A simple 2D game engine base on pyglet',
