3617ab2f346a6c55cfb2f779a1ae674baec6b921
Vic <305880887@qq.com>
2018-06-20 16:21:38 +0800

    add text inheret from rectangle

--- pyleap/shape/text.py
+++ pyleap/shape/text.py
@@ -3,39 +3,21 @@ from pyleap.window import window
 from pyleap.color import color_to_tuple
 from pyleap.util import all_shapes
 
-from pyleap.shape.shape import Shape
-from pyleap.collision import CollisionMixin
-from pyleap.transform import Transform, TransformMixin
+from pyleap.shape.rectangle import Rectangle
 
 
-class Text(pyglet.text.Label, CollisionMixin, TransformMixin):
+class Text(Rectangle):
     """ Text """
 
     def __init__(self, text, x=window.center_x, y=window.center_y,
                  font_size=16, color="orange"):
-        color = color_to_tuple(color)
-        super().__init__(text=text, x=x, y=y, font_size=font_size, color=color)
-        self.transform = Transform()
-        self.press_events = []
 
-    def update_points(self):
-        min_x = self.x
-        min_y = self.y
-        max_x = self.x + self.font_size * len(self.text) * 0.5
-        # width/height = 0.5
-        max_y = self.y + self.font_size
-
-        self.points = (min_x, min_y, max_x, min_y, max_x, max_y, min_x, max_y)
-
-    def update_anchor(self):
-        pass
+        self.text = pyglet.text.Label(text=text, font_size=font_size)
+        super().__init__(x=x, y=y, w=font_size*len(text)*0.6, h=font_size, color=color)
 
     def draw(self):
-        self.update_points()
-        self.update_anchor()
-
-        pyglet.gl.glLoadIdentity()
-        self.transform.update_gl()
-        all_shapes.discard(self)
-        all_shapes.add(self)
-        super().draw()
+        self.update_all()
+        self.text.x = self.x
+        self.text.y = self.y
+        self.text.color = color_to_tuple(self.color)
+        self.text.draw()
---
+++ test/text_test.py
@@ -0,0 +1,15 @@
+from pyleap import *
+
+t = Text('Hello, world', color="red", font_size=30)
+r = Rectangle(t.x, t.y, t.w, t.h)
+
+def draw(dt):
+    window.clear()
+    t.rotation += 1
+    r.rotation += 1
+
+    r.stroke()
+    t.draw()
+
+schedule_interval(draw)
+run()
