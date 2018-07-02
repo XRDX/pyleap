5fa820a5e5e9e28a19b370ca7fd73b18d0dd4d1c
Vic <305880887@qq.com>
2018-07-03 07:15:26 +0800

    add comments

--- pyleap/shape/shape.py
+++ pyleap/shape/shape.py
@@ -1,3 +1,11 @@
+""" 基本图形类 Shape
+
+所有图形都继承与Shape，Shape拥有以下两个主要的属性
+1. Collision，主要用于检测碰撞以及点击事件
+2. Transform，用于图形的变换处理
+
+"""
+
 import pyglet
 from pyglet import gl
 
@@ -12,6 +20,10 @@ class Shape(CollisionMixin, TransformMixin):
 
     def __init__(self, x, y, color="orange", gl=gl.GL_LINE_LOOP,
                  line_width=1, point_size=1):
+        """ 默认参数
+        颜色 color: "orange"
+        线条粗细 line_width： 1
+        """
         self.x = x
         self.y = y
         self.color = color
@@ -24,14 +36,17 @@ class Shape(CollisionMixin, TransformMixin):
         self.press_events = []
 
     def draw(self):
+        """ 使用draw方法将图形绘制在窗口里 """
         self.update_all()
         self.vertex_list.draw(self.gl)
 
     def stroke(self):
+        """ 使用stroke方法将图形绘制在窗口里，仅对基本的几何图形有效 """
         self.update_all()
         self.vertex_list.draw(gl.GL_LINE_LOOP)
 
     def update_all(self):
+        """ 在绘制之前，针对形变进行计算，通过设置openGL的属性来达到绘制出变形的图形 """
         self.update_points()
         self.update_vertex_list()
         self.update_anchor()
@@ -47,10 +62,11 @@ class Shape(CollisionMixin, TransformMixin):
 
 
     def update_points(self):
-        """ translate shapes to points """
+        """ translate shapes to points，在子类中实现 """
         pass
 
     def update_vertex_list(self):
+        """ 使用pyglet来绘制基本图形之前，转为pyglet识别的属性 """
         color = color_to_tuple(self.color)
         length = len(self.points) // 2
         self.vertex_list = pyglet.graphics.vertex_list(
@@ -59,6 +75,7 @@ class Shape(CollisionMixin, TransformMixin):
             ('c4B'.format(len(color)), color * length))
 
     def update_anchor(self):
+        """ 如果是使用set_anchor_rate来设定锚点，那么就需要不停的更新锚点的位置 """
         t = self.transform
         self.update_collision_rect()
         if t.anchor_x_r and t.anchor_y_r:
