5399c13a64c696a80c522b645734eb882546b577
Vic <305880887@qq.com>
2018-07-03 14:50:39 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyleap",
-    version="0.0.9",
+    version="0.1.0",
     author="Vic Wang",
     author_email="305880887@qq.com",
     description='A simple 2D game engine base on pyglet',
