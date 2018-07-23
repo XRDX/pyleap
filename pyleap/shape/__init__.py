36c6c501292c78889c2ddbe0ad5f6d52d3ddc6f7
Vic <305880887@qq.com>
2018-07-23 15:34:45 +0800

    update sprite

--- pyleap/shape/__init__.py
+++ pyleap/shape/__init__.py
@@ -3,6 +3,6 @@ from pyleap.shape.line import Line
 from pyleap.shape.point import Point
 from pyleap.shape.rectangle import Rectangle
 from pyleap.shape.shape import Shape
-from pyleap.shape.sprite import Sprite
+from pyleap.shape.sprite import Sprite, Batch
 from pyleap.shape.text import Text
 from pyleap.shape.triangle import Triangle
--- pyleap/shape/sprite.py
+++ pyleap/shape/sprite.py
@@ -7,6 +7,7 @@ from pyleap.shape.rectangle import Rectangle
 
 cache_images = {}
 
+
 class Sprite(Rectangle):
     """ Sprite """
 
@@ -14,16 +15,34 @@ class Sprite(Rectangle):
         """
         默认位置： 300, 200
         """
+
         if src not in cache_images:
               img = pyglet.image.load(rss.get(src))
               self.center_image(img)
               cache_images[src] = img
         
         self.img = cache_images[src]
-        self.sprite = pyglet.sprite.Sprite(img=self.img, x=x, y=y)
-        # self.collision_scale = 0.8
-        super().__init__(x, y)      
+        self._sprite = pyglet.sprite.Sprite(img=self.img)
 
+        super().__init__(x, y)
+        # self.collision_scale = 0.8
+
+
+    @property
+    def x(self):
+        return self._sprite.x
+
+    @x.setter
+    def x(self, x):
+        self._sprite.x = x
+
+    @property
+    def y(self):
+        return self._sprite.y
+
+    @y.setter
+    def y(self, y):
+        self._sprite.y = y
 
     def center_image(self, img):
         """Sets an image's anchor point to its center"""
@@ -42,6 +61,7 @@ class Sprite(Rectangle):
 
     def draw(self):
         self.update_all()
-        self.sprite.x = self.x
-        self.sprite.y = self.y
-        self.sprite.draw()
+        self._sprite.draw()
+
+    def delete(self):
+        self._sprite.delete()
