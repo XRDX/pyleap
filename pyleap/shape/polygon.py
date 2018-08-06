6a938d54427c29a997b95cbdc440ef80a983c090
yanpeng1314 <2677345028@qq.com>
2018-08-06 11:40:20 +0800

    modify polygon and test star of polygon.

--- pyleap/shape/polygon.py
+++ pyleap/shape/polygon.py
@@ -2,12 +2,10 @@ from pyglet import gl
 from pyleap.shape.shape import Shape
 import math
 
-
 class Polygon(Shape):
     """
     多边形：Polygon
     """
-
     def __init__(self, *args):
 
         """ 多边形
@@ -17,19 +15,15 @@ class Polygon(Shape):
 
         if isinstance(args[-1], str):
             color = args[-1]
-            x = sum(args[0: len(args)-1: 2]) / ((len(args) - 1) / 2)
-            y = sum(args[1: len(args)-1: 2]) / ((len(args) - 1) / 2)
+            points = args[:len(args)-1]
 
         else:
             color = "orange"
-            x = sum(args[0: len(args): 2]) / (len(args) / 2)
-            y = sum(args[1: len(args): 2]) / (len(args) / 2)
+            points = args
 
+        x = sum(points[::2]) / (len(points) / 2)
+        y = sum(points[1::2]) / (len(points) / 2)
 
         super().__init__(x, y, color, gl=gl.GL_POLYGON)
 
-        if isinstance(args[-1], str):
-            self.points = args[0: len(args)-1]
-        else:
-            self.points = args
-
+        self.points = points
--- test/test_polygon.py
+++ test/test_polygon.py
@@ -5,6 +5,8 @@ window.set_size(600, 600)
 t1 = Polygon(200, 100, 400, 100, 350, 300, 250, 300, "gray")
 t2 = Polygon(300, 200, 500, 200, 450, 500, 350, 500, 200, 600)
 
+star = Polygon(175, 300, 250, 100, 50, 225, 275, 225, 75, 100)
+
 def Loop(dt):
     window.clear()
 
@@ -16,6 +18,7 @@ def Loop(dt):
     if p:
         Point(p[0], p[1], color="red", point_size=5).draw()
 
+    star.draw()
 
 repeat(Loop)
 
