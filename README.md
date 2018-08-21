752ebdd15f7e1d17bd3738adbad53fcd829ab673
Vic <305880887@qq.com>
2018-08-21 19:18:42 +0800

    upload leap_server to pypi

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # pyleap: A simple 2D game engine base on pyglet
 
+install
+
+```shell
+pip install pyleap
+pip install leap-server
+```
+
+usage
+
+```shell
+python -m leap_server
+```
+
+注意，安装包使用的是`-`，运行本地服务器使用的是下划线`_`。
+
+
+
+
+
 upload to pypi
+
 ```shell
 $ python setup.py sdist bdist_wheel
 $ twine upload dist/* -u VicWang -p PYPI******
---
+++ server/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name="leap_server",
+    version="0.0.1",
+    author="Vic Wang",
+    author_email="305880887@qq.com",
+    description='a server for run python on web',
+    long_description="a server for run python on web",
+    long_description_content_type="text/markdown",
+    url='http://github.com/XRDX/pyleap',
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)
