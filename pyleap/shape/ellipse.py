a04d78197963f3e9894d7951432e17bc95275fa8
Vic <305880887@qq.com>
2018-10-23 16:56:02 +0800

    update ellipse comments

--- pyleap/shape/ellipse.py
+++ pyleap/shape/ellipse.py
@@ -15,9 +15,9 @@ class Ellipse(Shape):
 
     def __init__(self, x=100, y=100, r_x=50, r_y=30, color="orange"):
         """ 
-        圆心： x、y,    默认为100, 100
-        半径： r，     默认为30
-        颜色： color,  默认为 "orange"
+        圆心： x、y,        默认为100, 100
+        半径： r_x, r_y    默认为50, 30
+        颜色： color,      默认为 "orange"
         """
         super().__init__(x, y, color, gl=gl.GL_POLYGON)
         self.r_x = r_x
