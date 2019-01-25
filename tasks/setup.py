234c722cfbe66814254ab0d8f67d16b0b774f4d5
LLCS <funk.zhang@leaplearner.com>
2019-01-25 15:23:53 +0800

    update leaptask to 0.0.17

--- tasks/setup.py
+++ tasks/setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="leaptask",
-    version="0.0.16",
+    version="0.0.17",
     author="Vic Wang",
     author_email="305880887@qq.com",
     description='leap task',
